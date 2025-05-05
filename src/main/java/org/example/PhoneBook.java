a10a5d19af01d82cbaf69efb9605b39cbc997fe9
v.ionov <v.ionov@mail.ru>
2025-05-05 15:58:01 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class PhoneBook {
+
+    public int add(int number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,19 @@
-public class PhoneBookTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Test;
+
+class PhoneBookTest {
+
+    @Test
+    void addReturnsTwoWhenInputIsTwo() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(2, phoneBook.add(2));
+    }
+
+    @Test
+    void addReturnsOneWhenInputIsOne() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add(1));
+    }
 }
